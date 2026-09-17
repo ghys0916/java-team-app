d24bd0870f4f6bdbfeaf50d77dab9b32f700c860
3031028 <3031028@30310-28>
2026-09-17 14:44:53 +0900

    Merge branch 'master' of https://github.com/ghys0916/java-team-app.git

--- src/main/java/com/example/teamapp/controller/TeamController.java
+++ src/main/java/com/example/teamapp/controller/TeamController.java
@@ -1,14 +1,22 @@
 package com.example.teamapp.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.teamapp.service.KitayamaService;
+import com.example.teamapp.service.NakamuraService;
+
 @Controller
 public class TeamController {
+    @Autowired private KitayamaService kitayamaService; // メンバーのServiceを注入
+    @Autowired private NakamuraService nakamuraService; // メンバーのServiceを注入
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("leaderMsg", "リーダーです！！");
-        // TODO: ここにメンバーのServiceを後で呼ぶ
+        model.addAttribute("leaderMsg", "リーダーです！");
+        model.addAttribute("KitayamaMsg", kitayamaService.getProfile()); // 画面に渡す
+        model.addAttribute("NakamuraMsg", nakamuraService.getProfile()); // 画面に渡す
         return "index";
     }
 }
